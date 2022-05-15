cf9e951e4dc49ecb06c64b61ad80ce66f0adbdb3
thund1r <thuncoder@foxmail.com>
2022-05-16 00:27:27 +0800

    修复签到失败与异常情况下推送通知失败的bug

--- api/glados.py
+++ api/glados.py
@@ -19,28 +19,24 @@ def checkin():
                                     'user-agent': useragent, 'content-type': 'application/json;charset=UTF-8'}, data=json.dumps(payload)).json()
             state = requests.get(url2, headers={
                 'cookie': cookie, 'referer': referer, 'origin': origin, 'user-agent': useragent}).json()
+            message = checkin['message']
             if checkin['code'] == 0 or (checkin['code'] == 1 and 'list' in checkin):
                 email = state['data']['email']
                 time = state['data']['leftDays'].split('.')[0]
-                message = checkin['message']
-                title = "Glados签到成功"
+                title = "Glados签到成功，" + message
                 content = "账号：" + email + "\n\n剩余天数："+time + \
                     "天\n\n成功信息：" + message + "\n\n - - - "
                 push.push_msg(title, content)
                 return {"code": 200, "msg": title}
             else:
-                email = state['data']['email']
-                time = state['data']['leftDays'].split('.')[0]
-                message = checkin['message']
-                title = "Glados签到失败"
-                content = "账号：" + email + "\n\n剩余天数："+time + \
-                    "天\n\n失败信息：" + message + "\n\n - - - "
+                title = "Glados签到失败，" + message
+                content = "错误信息：" + message + "，请尝试检查或更新参数。\n\n - - - "
                 push.push_msg(title, content)
                 return {"code": 401, "msg": title}
         except Exception as errorMsg:
             print("Glados签到异常:", errorMsg)
-            title = "Glados签到异常"
-            content = "异常信息：" + str(errorMsg) + "，请检查控制台报错信息。\n\n - - - "
+            title = "Glados签到异常，" + repr(errorMsg)
+            content = "异常信息：" + repr(errorMsg) + "，请检查控制台报错信息。\n\n - - - "
             push.push_msg(title, content)
             return {"code": 500, "msg": title}
     else:
